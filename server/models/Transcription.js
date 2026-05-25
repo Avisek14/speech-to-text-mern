1ad3900990f35fe552c22d43b7c16f71a8f10bc4
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-05-26 01:43:26 +0530

    Day-2 Transcription schema setup and ubdate the server.js

---
+++ server/models/Transcription.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const transcriptionSchema = new mongoose.Schema(
+  {
+    audioFileName: {
+      type: String,
+      required: true,
+    },
+    transcriptionText: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: false, // baad me auth add hone ke baad true karenge
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Transcription', transcriptionSchema);
