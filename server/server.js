dff336078bf38cdaaf1395c2e0e07df87f61f1e7
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-05-26 02:18:08 +0530

    Day-2 update server.js for Route import and Route use

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
+const transcriptionRoutes = require('./routes/transcriptionRoutes');
 
 // Connect to MongoDB
 connectDB();
@@ -10,6 +11,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api', transcriptionRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Speech-to-Text API is running 🚀' });
