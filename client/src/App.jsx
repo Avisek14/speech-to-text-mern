74246fcb25c403e8cbe46cf40c82de9dc48c922c
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-05-28 03:36:14 +0530

    Day-5 update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+import { useAuth } from './context/AuthContext'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+  const { user } = useAuth()
+  const [showLogin, setShowLogin] = useState(true)
+
+  if (!user) {
+    return showLogin
+      ? <Login onSwitch={() => setShowLogin(false)} />
+      : <Register onSwitch={() => setShowLogin(true)} />
+  }
+
+  return <Home />
 }
 
 export default App
