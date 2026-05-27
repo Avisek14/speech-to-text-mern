d4c2b0b70ce36432a27609c9b5c6cc893c1ef4fe
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-05-28 03:26:51 +0530

    Day-5 update server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const transcriptionRoutes = require('./routes/transcriptionRoutes');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const transcriptionRoutes = require('./routes/transcriptionRoutes')
+const authRoutes = require('./routes/authRoutes')
 
-// Connect to MongoDB
-connectDB();
+connectDB()
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.use('/api', transcriptionRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api', transcriptionRoutes)
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Speech-to-Text API is running 🚀' });
-});
+  res.json({ message: 'VoiceScript API is running 🚀' })
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
